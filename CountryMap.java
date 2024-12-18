718a3173b752ee6ec9ae80a5df18b2767e98e898
Kutluay Aydın <100884457+Kutluay684@users.noreply.github.com>
2024-12-18 21:44:59 +0300

    Update CountryMap.java
    Since I couldn't use ArrayList, I used two-dimensional arrays and created methods for adding cities and routes. I found the city name and converted it to an index.

--- CountryMap.java
+++ CountryMap.java
@@ -1,23 +1,46 @@
-import java.util.*;
 
 public class CountryMap {
-    private Map<String, City> cities = new HashMap<>();
-    private Map<String, List<Route>> routes = new HashMap<>();
 
-    public void addCity(String cityName) {
-        cities.put(cityName, new City(cityName));
+
+    String[] cities ;
+    int[][] routes;
+
+     public CountryMap(int numberOfCities) {
+         cities = new String[numberOfCities];
+         routes = new int[numberOfCities][numberOfCities];
+
+         for (int i = 0; i < numberOfCities; i++) {
+             for (int j = 0; j < numberOfCities; j++) {
+
+
+                   routes[j][i]=99999999;
+             }
+
+         }
+     }
+    public void addCity(int index, String label) {
+        cities[index] = label;
     }
-
-    public void addRoute(String city1, String city2, int time) {
-        routes.computeIfAbsent(city1, k -> new ArrayList<>()).add(new Route(city1, city2, time));
-        routes.computeIfAbsent(city2, k -> new ArrayList<>()).add(new Route(city2, city1, time));
+    public void addRoute(String city1, String city2,int time)
+    {
+int index1=findCityIndex(city1);
+int index2=findCityIndex(city2);
+if (index1!=-1 &&  index2!= -1)
+{
+  routes[index1][index2]=time;
+  routes[index2][index1]=time;
+}
     }
-
-    public List<Route> getRoutes(String city) {
-        return routes.getOrDefault(city, new ArrayList<>());
-    }
-
-    public boolean cityExists(String city) {
-        return cities.containsKey(city);
+    public int findCityIndex(String city)
+    {
+         for(int i=0; i<cities.length;i++)
+         {
+             if (cities[i].equals(city))
+             {
+return i;
+             }
+            return -1;
+         }
+        return 0;
     }
 }
