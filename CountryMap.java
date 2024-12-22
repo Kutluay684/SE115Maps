aea6e606c6acffb56868b0f6513aede23d70e109
Kutluay Aydın <100884457+Kutluay684@users.noreply.github.com>
2024-12-22 22:14:07 +0300

    Update CountryMap.java
    I fixed the errors regarding the usage of the City class within the program.

--- CountryMap.java
+++ CountryMap.java
@@ -1,46 +1,37 @@
+class CountryMap {
+   City[] cities;
+   int[][] routes;
 
-public class CountryMap {
+   public CountryMap(int numberOfCities) {
+      this.cities = new City[numberOfCities];
+      this.routes = new int[numberOfCities][numberOfCities];
 
-
-    String[] cities ;
-    int[][] routes;
-
-     public CountryMap(int numberOfCities) {
-         cities = new String[numberOfCities];
-         routes = new int[numberOfCities][numberOfCities];
-
-         for (int i = 0; i < numberOfCities; i++) {
-             for (int j = 0; j < numberOfCities; j++) {
-
-
-                   routes[j][i]=99999999;
-             }
-
+      for (int i = 0; i < numberOfCities; ++i) {
+         for (int j = 0; j < numberOfCities; ++j) {
+            this.routes[i][j] = Integer.MAX_VALUE;
          }
-     }
-    public void addCity(int index, String label) {
-        cities[index] = label;
-    }
-    public void addRoute(String city1, String city2,int time)
-    {
-int index1=findCityIndex(city1);
-int index2=findCityIndex(city2);
-if (index1!=-1 &&  index2!= -1)
-{
-  routes[index1][index2]=time;
-  routes[index2][index1]=time;
-}
-    }
-    public int findCityIndex(String city)
-    {
-         for(int i=0; i<cities.length;i++)
-         {
-             if (cities[i].equals(city))
-             {
-return i;
-             }
-            return -1;
+      }
+   }
+
+   public void addCity(int index, String label) {
+      this.cities[index] = new City(label);
+   }
+
+   public void addRoute(String city1, String city2, int time) {
+      int index1 = this.findCityIndex(city1);
+      int index2 = this.findCityIndex(city2);
+      if (index1 != -1 && index2 != -1) {
+         this.routes[index1][index2] = time;
+         this.routes[index2][index1] = time;
+      }
+   }
+
+   public int findCityIndex(String city) {
+      for (int i = 0; i < this.cities.length; ++i) {
+         if (this.cities[i].getName().equals(city)) {
+            return i;
          }
-        return 0;
-    }
+      }
+      return -1;
+   }
 }
